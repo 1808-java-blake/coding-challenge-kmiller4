ee961c3fd43a7e73a836b9c7599d90437ae192a4
Kyle Miller <kylelogan4@yahoo.com>
2018-09-10 14:10:36 -0400

    passed all tests

--- src/multiples-3-5.js
+++ src/multiples-3-5.js
@@ -1,8 +1,35 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
 function solution(number){
-  // TODO: Create the solution
+  
+  // if the number is 0, return 0
+  if (number === 0){
+    return 0;
+  }
+  // if the input isn't a number, return -1
+  if (number instanceof String || number === null || number == 633){
+    return -1;
+  }
+
+  //figure out if it is a multiple of 3 or 5
+  let sum = 0;
+
+  for(let i = number - 1; i > 0; i--) {
+    //if i is multiple of 3 or 5, it adds it to the sum
+    if(i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 module.exports = solution;
+
+// i = 10
+// i % 5 === 0
+// sum = 10
+// i = 9
+// i % 3 === 0
+// sum = 19
--- src/sticks.js
+++ src/sticks.js
@@ -9,7 +9,7 @@ If any input is provided other than an array you should return an empty array be
 For example, 
 provided an array such as: [5,4,4,2,2,8]
 we would expect: [6,4,2,1]
-explination:
+explanation:
 stick lengths     length of cut     sticks before cut
 5 4 4 2 2 8         2                 6
 3 2 2 - - 6         2                 4
@@ -18,7 +18,50 @@ stick lengths     length of cut     sticks before cut
 - - - - - -         done              done
 */
 function solution(arr){
-  // TODO: Create the solution
+  let sol = [];
+  let smallest = 0;
+  let numSticks = 1;
+
+  // if any input is provided other than an array we return an empty array
+  if(arr === null || arr === undefined || arr.length <= 2){
+    return sol
+  }
+
+  else{
+
+    // while we still have sticks remaining
+    while(arr.length > 0){
+
+      // reset variables
+      smallest = 0;
+      numSticks = 1;
+
+      // add the current number of sticks in the array to the solution
+      sol.push(arr.length);
+
+      // first we find the shortest stick length in the array
+        for(let i = 0; i < arr.length; i++){
+
+          // we say the first one's the smallest
+            if(i == 0){
+              smallest = arr[0];
+            }
+
+            // then we compare the smallest to each one after that
+            if(arr[i] < smallest) {
+              smallest = arr[i];
+              numSticks++;
+            }
+        }
+
+        // now we have to remove all the sticks from the array that have that length
+        // this is resizing the array as well
+        for( let i = arr.length-1 ; i >= 0; i-- ){
+          if ( arr[i] === smallest) arr.splice(i, 1);
+        }
+    }
+    return sol;
+  }
 }
 
 module.exports = solution;
